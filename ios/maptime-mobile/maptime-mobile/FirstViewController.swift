30fd8ef067907d12ff639b242c3b388c2c58fc14
Nadia Barbosa <nadiabarbosa@me.com>
2019-04-10 11:49:46 -0700

    Add iOS code

--- ios/maptime-mobile/maptime-mobile/FirstViewController.swift
+++ ios/maptime-mobile/maptime-mobile/FirstViewController.swift
@@ -1,14 +1,42 @@
 import UIKit
+import Mapbox
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, MGLMapViewDelegate {
+
+    var mapView: MGLMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print("Loaded first")
+        // ⭐️ Challenge: Customize the Mapbox Style
+        // MGLStyle.streetsStyleURL is a conveninent shorthand our SDK provides that points to
+        // the most current Mapbox Streets style.
+        // (API reference: https://docs.mapbox.com/ios/api/maps/4.9.0/Classes/MGLStyle.html)
+        // Can you change the style URL below to point to your custom Mapbox Studio Style?
+        // Example reference: https://docs.mapbox.com/ios/maps/examples/custom-style/
+        mapView = MGLMapView(frame: view.frame, styleURL: MGLStyle.streetsStyleURL)
+        mapView.delegate = self
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+        // ⭐️ Challenge: Set the center coordinate and zoom level
+        // This map currently doesn't have a specified center or zoom level.
+        // Can you change the map view so that it centers on your hometown?
+        // API reference: https://docs.mapbox.com/ios/api/maps/4.9.0/Classes/MGLMapView.html#/Manipulating%20the%20Viewpoint
+        // Example reference: https://docs.mapbox.com/ios/maps/examples/simple-map-view/
+
+
+        // ⭐️ Challenge: Add a basic marker
+        // Can you add a marker at the center of the map?
+        // The most basic way to add a marker is using MGLPointAnnotation.
+        // API reference: https://docs.mapbox.com/ios/api/maps/4.9.0/Classes/MGLPointAnnotation.html
+        // Example: https://docs.mapbox.com/ios/maps/examples/marker/
+        view.addSubview(mapView)
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        print("Presented first")
-    }
+    // This function tells any MGLPointAnnotation to display a callout. Uncomment these lines
+    // if you want to show a callout when you tap on your marker.
+    
+//    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+//        return true
+//    }
 }
