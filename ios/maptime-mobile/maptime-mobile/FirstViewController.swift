d1a35860836974b950c1309d1ef36c0ccc8c73a8
Nadia Barbosa <nadiabarbosa@me.com>
2019-04-10 11:59:19 -0700

    Code comment formatting

--- ios/maptime-mobile/maptime-mobile/FirstViewController.swift
+++ ios/maptime-mobile/maptime-mobile/FirstViewController.swift
@@ -10,10 +10,9 @@ class FirstViewController: UIViewController, MGLMapViewDelegate {
 
         // ⭐️ Challenge: Customize the Mapbox Style
         // MGLStyle.streetsStyleURL is a conveninent shorthand our SDK provides that points to
-        // the most current Mapbox Streets style.
-        // (API reference: https://docs.mapbox.com/ios/api/maps/4.9.0/Classes/MGLStyle.html)
+        // the most current Mapbox Streets style (see https://docs.mapbox.com/ios/api/maps/4.9.0/Classes/MGLStyle.html).
         // Can you change the style URL below to point to your custom Mapbox Studio Style?
-        // Example reference: https://docs.mapbox.com/ios/maps/examples/custom-style/
+        // • Example reference: https://docs.mapbox.com/ios/maps/examples/custom-style/
         mapView = MGLMapView(frame: view.frame, styleURL: MGLStyle.streetsStyleURL)
         mapView.delegate = self
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
@@ -21,15 +20,15 @@ class FirstViewController: UIViewController, MGLMapViewDelegate {
         // ⭐️ Challenge: Set the center coordinate and zoom level
         // This map currently doesn't have a specified center or zoom level.
         // Can you change the map view so that it centers on your hometown?
-        // API reference: https://docs.mapbox.com/ios/api/maps/4.9.0/Classes/MGLMapView.html#/Manipulating%20the%20Viewpoint
-        // Example reference: https://docs.mapbox.com/ios/maps/examples/simple-map-view/
+        // • API reference: https://docs.mapbox.com/ios/api/maps/4.9.0/Classes/MGLMapView.html#/Manipulating%20the%20Viewpoint
+        // • Example reference: https://docs.mapbox.com/ios/maps/examples/simple-map-view/
 
 
         // ⭐️ Challenge: Add a basic marker
         // Can you add a marker at the center of the map?
         // The most basic way to add a marker is using MGLPointAnnotation.
-        // API reference: https://docs.mapbox.com/ios/api/maps/4.9.0/Classes/MGLPointAnnotation.html
-        // Example: https://docs.mapbox.com/ios/maps/examples/marker/
+        // • API reference: https://docs.mapbox.com/ios/api/maps/4.9.0/Classes/MGLPointAnnotation.html
+        // • Example reference: https://docs.mapbox.com/ios/maps/examples/marker/
         view.addSubview(mapView)
     }
 
